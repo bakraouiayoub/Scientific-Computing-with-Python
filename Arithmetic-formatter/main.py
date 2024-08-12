265f5828260f388b59d8c3899098a881e4e67bbf
Ayoub Bakraoui <88404680+bakraouiayoub@users.noreply.github.com>
2024-08-12 13:42:59 -0400

    Update main.py

--- Arithmetic-formatter/main.py
+++ Arithmetic-formatter/main.py
@@ -1,4 +1,4 @@
-from arithmetic-formatter import arithmetic_arranger
+from arithmetic_formatter import arithmetic_arranger
 first_problem="3801 - 2000"
 second_problem="123 + 1890"
 third_problem="5000 - 6500"
