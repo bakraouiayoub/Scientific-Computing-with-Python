e1c77e1b9cc47c2f29a2171bebe9b60e06c2ab9b
Ayoub Bakraoui <88404680+bakraouiayoub@users.noreply.github.com>
2024-08-12 13:16:27 -0400

    Update main.py

--- Budget tracker/main.py
+++ Budget tracker/main.py
@@ -1,4 +1,4 @@
-from budget_tracker.py import 
+from budget_tracker.py import Category, create_spend_chart
 food = Category('Food') 
 food.deposit(1000, 'initial deposit')
 food.withdraw(10.15, 'groceries')
