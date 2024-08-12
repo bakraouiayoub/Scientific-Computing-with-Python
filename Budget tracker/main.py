7b15ad0fd6fe0f9649e7bc44febe797f0b0603e0
Ayoub Bakraoui <88404680+bakraouiayoub@users.noreply.github.com>
2024-08-12 13:14:54 -0400

    Create main.py

---
+++ Budget tracker/main.py
@@ -0,0 +1,20 @@
+from budget_tracker.py import 
+food = Category('Food') 
+food.deposit(1000, 'initial deposit')
+food.withdraw(10.15, 'groceries')
+food.withdraw(15.89, 'restaurant and more food for dessert') 
+clothing = Category('Clothing')
+food.transfer(400, clothing)
+clothing.withdraw(200,'suit')
+auto = Category('Auto')
+auto.deposit(1500,'initial deposit')
+auto.withdraw(400,'Repairs')
+print(food)
+print('\n')
+print(clothing)
+print('\n')
+print(auto)
+print('\n')
+print(create_spend_chart([food,clothing,auto]))
+  
+
